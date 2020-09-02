0a553cfb5eb3058e36b533f242b7235ab09e74a9
JhonatanWalterSen <jhonatanarizaq@gmail.com>
2020-09-02 01:40:00 -0500

    Validando formulario

--- js/app.js
+++ js/app.js
@@ -1,3 +1,94 @@
+// variables
+const btnEnviar = document.querySelector('.btnEnviar');
+const formulario = document.querySelector('#enviarMail')
+// Variables para campos
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+const  er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+eventListeners();
+function eventListeners(){
+    // Cuando la App inicia
+    document.addEventListener('DOMContentLoaded', iniciarApp)
+
+    // Campos del formulario
+    nombre.addEventListener('blur', validarFormulario)
+    email.addEventListener('blur', validarFormulario)
+    asunto.addEventListener('blur', validarFormulario)
+    mensaje.addEventListener('blur', validarFormulario)
+    // enviar formulario
+    formulario.addEventListener('click', enviarEmail)
+}
+
+// Funciones
+function iniciarApp(){
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('opacity-50');
+}
+//  Valida el fornulario
+function validarFormulario(e){
+    console.log(e.target.value)
+    if(e.target.value.length > 0){
+
+        // elimina los errores
+        const error = document.querySelector('p.error');
+        if(error){
+            error.remove();
+        }
+
+        e.target.style.borderBottomColor= 'none';
+        e.target.style.borderBottomColor= '#ffce00';
+    }else{
+        e.target.style.borderBottomColor= '#e84855';
+        e.target.style.borderBottomColor= 'none';
+        mostrarError('Todos los campos son obligatorios');
+    }
+    
+    if(e.target.type === 'email'){
+            if(er.test(e.target.value)){
+            // elimina los errores
+            const error = document.querySelector('p.error');
+            if(error){
+                error.remove();
+            }
+
+            e.target.style.borderBottomColor= 'none';
+            e.target.style.borderBottomColor= '#ffce00';
+        }else{
+            e.target.style.borderBottomColor= '#e84855';
+            e.target.style.borderBottomColor= 'none';
+            mostrarError('Email no valido');
+        }
+    }
+
+    if(nombre.value !== '' && er.test(email.value) && asunto.value !=='' && mensaje.value !==''){
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('opacity-50');
+    }else{
+
+    }
+}
+function mostrarError(mensaje){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('borderror', 'error')
+
+    const errores = document.querySelectorAll('.error')
+    if(errores.length === 0){
+       formulario.appendChild(mensajeError) 
+    }
+}
+
+
+function enviarEmail(e){
+    e.preventDefault();
+    console.log('Enviando')
+}
+
+
 function menuMobile() {
     let change = 'change';
     let none = 'd-none';
