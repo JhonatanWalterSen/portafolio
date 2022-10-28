d19db7e82e571aecacd8189d14127f198e50ffa3
Jhonatan Ariza <arizaj@LIM915010.local>
2022-10-28 07:08:19 -0500

    fixed formulario

--- js/app.js
+++ js/app.js
@@ -1,67 +1,57 @@
 // variables
-const btnEnviar = document.querySelector('.btnEnviar');
-const formulario = document.querySelector('#enviarMail')
+const btnEnviar = document.querySelector('.btnEnviar'),
+    formulario = document.querySelector('#enviarMail')
 // Variables para campos
-const nombre = document.querySelector('#nombre');
-const email = document.querySelector('#email');
-const asunto = document.querySelector('#asunto');
-const mensaje = document.querySelector('#mensaje');
-const  er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const nombre = document.querySelector('#nombre'),
+    email = document.querySelector('#email'),
+    asunto = document.querySelector('#asunto'),
+    mensaje = document.querySelector('#mensaje'),
+    er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', iniciarApp)
-    nombre.addEventListener('blur', validarFormulario)
-    email.addEventListener('blur', validarFormulario)
-    asunto.addEventListener('blur', validarFormulario)
-    mensaje.addEventListener('blur', validarFormulario)
-    formulario.addEventListener('click', enviarEmail)
+    nombre.addEventListener('input', validarFormulario)
+    email.addEventListener('input', validarFormulario)
+    asunto.addEventListener('input', validarFormulario)
+    mensaje.addEventListener('input', validarFormulario)
+    btnEnviar.addEventListener('click', enviarEmail)
 }
 
 // Funciones
 function iniciarApp(){
     btnEnviar.disabled = true;
-    btnEnviar.classList.add('opacity-50');
 }
 
 function validarFormulario(e){
-    console.log(e.target.value)
-    if(e.target.value.length > 0){
-        const error = document.querySelector('p.error');
-        if(error){
-            error.remove();
-        }
+    const error = document.querySelector('.borderror');
 
-        e.target.style.borderBottomColor= 'none';
-        e.target.style.borderBottomColor= '#ffce00';
-    }else{
+    if (e.target.value.trim() === '') {
+        mostrarError('Campos Obligatorios')
         e.target.style.borderBottomColor= '#e84855';
-        e.target.style.borderBottomColor= 'none';
-        mostrarError('Todos los campos son obligatorios');
+        btnEnviar.disabled = true;
+        btnEnviar.classList.remove('activoBtn')
+        return
     }
 
-    if(e.target.type === 'email'){
-            if(er.test(e.target.value)){
-            const error = document.querySelector('p.error');
-            if(error){
-                error.remove();
-            }
-
-            e.target.style.borderBottomColor= 'none';
-            e.target.style.borderBottomColor= '#ffce00';
-        }else{
-            e.target.style.borderBottomColor= '#e84855';
-            e.target.style.borderBottomColor= 'none';
-            mostrarError('Email no valido');
-        }
+    if (e.target.type === 'email' && !er.test(email.value)) {
+        mostrarError('El email no es válido')
+        e.target.style.borderBottomColor= '#e84855';
+        btnEnviar.disabled = true;
+        btnEnviar.classList.remove('activoBtn')
+        return
     }
+
+    e.target.style.borderBottomColor= '#58a345';
+    limpiarError(error)
 
     if(nombre.value !== '' && er.test(email.value) && asunto.value !=='' && mensaje.value !==''){
+        btnEnviar.classList.add('activoBtn')
         btnEnviar.disabled = false;
-        btnEnviar.classList.remove('opacity-50');
-    }else{
+        return
     }
+
 }
 function mostrarError(mensaje){
     const mensajeError = document.createElement('p');
@@ -74,12 +64,22 @@ function mostrarError(mensaje){
     }
 }
 
-
-function enviarEmail(e){
-    e.preventDefault();
-    console.log('Enviando')
+function limpiarError(error) {
+    if (error) {
+        error.remove();
+    }
 }
 
+function enviarEmail(){
+    Swal.fire(
+        'Correo Enviado con Éxito',
+        '',
+        'success'
+    )
+    formulario.reset()
+    btnEnviar.disabled = true;
+    btnEnviar.classList.remove('activoBtn')
+}
 
 function menuMobile() {
     let change = 'change';
@@ -89,7 +89,6 @@ function menuMobile() {
     const brand = document.querySelector('.brand');
     const menuEstru = document.querySelector('.navbar .container');
     const items = document.querySelector('.items');
-    let imenu = document.querySelector('.menu-tablet-mobile');
         menuOn.addEventListener('click', () => {
             if (brand.classList.contains(none)) {
                 brand.classList.remove(none)
